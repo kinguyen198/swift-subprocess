419adfa8734724293094a024651d04f5a76b1b93
kinguyen198 <67310086+kinguyen198@users.noreply.github.com>
2025-07-11 02:42:22 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 var dep: [Package.Dependency] = [
     .package(
         url: "https://github.com/apple/swift-system",
-        from: "1.4.2"
+        from: "1.1.1"
     )
 ]
 #if !os(Windows)
